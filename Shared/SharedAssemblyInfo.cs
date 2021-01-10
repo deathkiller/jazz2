79d6956944fbaf8fb509c4630cf5dd063ebec3fe
Death Killer <danielrajf@gmail.com>
2021-01-10 14:15:19 +0100

    Updated version

--- Shared/SharedAssemblyInfo.cs
+++ Shared/SharedAssemblyInfo.cs
@@ -5,9 +5,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Jazz² Resurrection")]
-[assembly: AssemblyCopyright("© 2016-2020 Dan R.")]
+[assembly: AssemblyCopyright("© 2016-2021 Dan R.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.6.5.0")]
-[assembly: AssemblyFileVersion("0.6.5.0")]
+[assembly: AssemblyVersion("0.6.6.0")]
+[assembly: AssemblyFileVersion("0.6.6.0")]
