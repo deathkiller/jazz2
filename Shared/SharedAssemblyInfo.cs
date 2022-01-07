034064ad8dfc539624f84c46aa3b79a0a6cd0137
Death Killer <danielrajf@gmail.com>
2022-01-07 17:47:54 +0100

    Updated version

--- Shared/SharedAssemblyInfo.cs
+++ Shared/SharedAssemblyInfo.cs
@@ -9,5 +9,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("0.6.8.0")]
-[assembly: AssemblyFileVersion("0.6.8.0")]
+[assembly: AssemblyVersion("0.7.0.0")]
+[assembly: AssemblyFileVersion("0.7.0.0")]
